8bf3e18788492e6e65a3b7f1e6655f2fc3737167
yikejason <18215513386@163.com>
2024-11-12 22:21:42 +0800

    feature: use derive_builder crate do something

---
+++ examples/builder.rs
@@ -0,0 +1,57 @@
+use anyhow::Result;
+use chrono::{DateTime, Datelike, Utc};
+use derive_builder::Builder;
+
+#[allow(unused)]
+#[derive(Debug, Builder)]
+#[builder(build_fn(name = "_priv_build"))]
+struct User {
+    #[builder(setter(into))]
+    name: String,
+    #[builder(setter(into, strip_option), default)]
+    email: Option<String>,
+    #[builder(setter(custom))]
+    dob: DateTime<Utc>,
+    #[builder(setter(skip))]
+    age: u32,
+    #[builder(default = "vec![]", setter(each(name = "skill", into)))]
+    skills: Vec<String>,
+}
+
+fn main() -> Result<()> {
+    let user = User::build()
+        .name("Alice")
+        .skill("programming")
+        .skill("debugging")
+        .email("yu@answesome.com")
+        .dob("1990-01-01T00:00:00Z")
+        .build()?;
+
+    println!("{:?}", user);
+
+    Ok(())
+}
+
+impl User {
+    pub fn build() -> UserBuilder {
+        UserBuilder::default()
+    }
+}
+
+impl UserBuilder {
+    pub fn build(&self) -> Result<User> {
+        let mut user = self._priv_build()?;
+        user.age = (Utc::now().year() - user.dob.year()) as u32;
+
+        Ok(user)
+    }
+
+    pub fn dob(&mut self, value: &str) -> &mut Self {
+        let date = DateTime::parse_from_rfc3339(value)
+            .map(|dt| dt.with_timezone(&Utc))
+            .ok();
+        self.dob = date;
+
+        self
+    }
+}
